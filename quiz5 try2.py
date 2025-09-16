f8aa40c125354bf51516a6387e7c2de54161ca3d
Tobioladipo01 <tobioladipo004@gmail.com>
2025-09-16 10:57:45 +0100

    final shopping list code

--- quiz5 try2.py
+++ quiz5 try2.py
@@ -1,21 +1,27 @@
 shoppinglist = []
+
 while True: 
     userinput = input ("do you want to view, add or remove from the current list? or do you want to quit this page? ").lower().strip()
-    if userinput == "view":
+
+    if "view" in userinput:
         print ("shoppinglist: ")
         for x in shoppinglist:
-            print(X)
+            print(x)
+
     elif userinput == "add":
         useritems = input ("Add an item to the list:")
         shoppinglist.append(useritems)
         print(f"{useritems} added to the list")
+
     elif userinput == "remove":
         userremove = input ("what item do you want to remove? ")
         shoppinglist.remove(userremove)
-        print(shoppinglist)
+        print(f"{userremove} has been removed from the list")
+
     elif userinput == "quit":
         print ("Thank you for using our software. Hope to see you next time")
         break
+
     else:
         print("enter a valid prompt")
     
